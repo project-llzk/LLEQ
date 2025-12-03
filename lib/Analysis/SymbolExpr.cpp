f0dde1c6a7a05a63c15a887e4b93e630bb4eeda1
Raghav Malik <malikraghav9@gmail.com>
2025-12-03 17:44:40 -0500

    Symbolic expressions to hold in the symbolic store  (#2)
    * Symbol expressions
    
    * Refactored to use std::pmr::polymorphic_allocator instead of custom Arena
    
    * Arrays can be multi-dimensional, or 0-dimensional
    
    ---------
    
    Co-authored-by: Raghav Malik <raghav@veridise.com>

---
+++ lib/Analysis/SymbolExpr.cpp
@@ -0,0 +1,105 @@
+#include "Analysis/SymbolExpr.h"
+
+#include <algorithm>
+#include <initializer_list>
+#include <llvm/Support/raw_os_ostream.h>
+#include <memory_resource>
+#include <mlir/Support/LLVM.h>
+#include <ostream>
+#include <vector>
+
+using namespace lleq;
+
+struct impl::SymbolBase {
+  virtual std::ostream &print(std::ostream &os) const = 0;
+};
+
+struct Unknown : impl::SymbolBase {
+  size_t n;
+  Unknown(size_t n) : impl::SymbolBase{}, n{n} {}
+  std::ostream &print(std::ostream &os) const override {
+    os << '?' << n;
+    return os;
+  }
+};
+
+struct Constant : impl::SymbolBase {
+  mlir::APInt value;
+  Constant(mlir::APInt value) : impl::SymbolBase{}, value{value} {}
+  std::ostream &print(std::ostream &os) const override {
+    // Print it as signed
+    llvm::raw_os_ostream ros(os);
+    value.print(ros, true);
+    return os;
+  }
+};
+
+struct TemplParam : impl::SymbolBase {
+  std::string_view name;
+  TemplParam(std::string_view name) : impl::SymbolBase{}, name{name} {}
+  std::ostream &print(std::ostream &os) const override {
+    os << '@' << name;
+    return os;
+  }
+};
+
+struct Index : impl::SymbolBase {
+  mlir::Value signal;
+  std::pmr::vector<Symbol> indices;
+
+  // Takes a pointer to the memory resource backing the SymbolPool so we can
+  // reuse it for the vector of indices
+  Index(std::pmr::memory_resource *memory, mlir::Value signal,
+        std::initializer_list<Symbol> ns)
+      : impl::SymbolBase{}, signal{signal} {
+    indices = std::pmr::vector<Symbol>{ns.begin(), ns.end(), memory};
+  }
+  std::ostream &print(std::ostream &os) const override {
+    // TODO: print the MLIR value too
+    os << "sig";
+    for (auto n : indices) {
+      os << "[";
+      n->print(os) << "]";
+    }
+    return os;
+  }
+};
+
+struct Arith : impl::SymbolBase {
+  Symbol lhs, rhs;
+  char op;
+
+  Arith(Symbol lhs, Symbol rhs, char op)
+      : impl::SymbolBase{}, lhs{lhs}, rhs{rhs}, op{op} {}
+  std::ostream &print(std::ostream &os) const override {
+    os << '(';
+    lhs->print(os) << op;
+    rhs->print(os) << ')';
+    return os;
+  }
+};
+
+Symbol SymbolPool::fresh_unknown() {
+  static std::size_t n;
+  return alloc.new_object<Unknown>(n++);
+}
+
+Symbol SymbolPool::constant(mlir::APInt value) {
+  return alloc.new_object<Constant>(value);
+}
+Symbol SymbolPool::templ_param(std::string_view name) {
+  return alloc.new_object<TemplParam>(name);
+}
+Symbol SymbolPool::index(mlir::Value signal, std::initializer_list<Symbol> ns) {
+  return alloc.new_object<Index>(&memory, signal, ns);
+}
+Symbol SymbolPool::arith(Symbol lhs, Symbol rhs, char op) {
+  if (std::find(ALLOWED_OPS.begin(), ALLOWED_OPS.end(), op) ==
+      ALLOWED_OPS.end()) {
+    // TODO: What's a better way of signaling an error here? An exception?
+    return nullptr;
+  }
+  return alloc.new_object<Arith>(lhs, rhs, op);
+}
+
+std::ostream &operator<<(std::ostream &os, Symbol s) { return s->print(os); }
--- tools/lleq/main.cpp
+++ tools/lleq/main.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
 #include <mlir/Support/LogicalResult.h>
 
+#include "Analysis/SymbolExpr.h"
+
 mlir::LogicalResult test() { return mlir::failure(); }
 
 int main(int argc, char **argv) {
+
+  lleq::SymbolPool pool;
+  lleq::Symbol expr = pool.arith(
+      pool.constant(mlir::APInt{8, 250}),
+      pool.arith(pool.fresh_unknown(), pool.templ_param("N"), '+'), '*');
+  lleq::Symbol expr2 = pool.index(mlir::Value::getFromOpaquePointer(nullptr),
+                                  {expr, pool.templ_param("M")});
+
   if (mlir::failed(test())) {
-    std::cout << "Good job\n";
+    std::cout << "Good job: " << expr2 << "\n";
   }
   return 0;
 }
