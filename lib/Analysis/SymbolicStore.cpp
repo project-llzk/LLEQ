ee39e19aa5462862169aa34b17ddd6edc60aad63
Raghav Malik <raghav@veridise.com>
2025-12-09 14:34:49 -0500

    Implemented lookup

--- lib/Analysis/SymbolicStore.cpp
+++ lib/Analysis/SymbolicStore.cpp
@@ -7,6 +7,7 @@
 #include <llzk/Dialect/Felt/IR/Dialect.h>
 #include <llzk/Dialect/Felt/IR/Ops.h>
 #include <llzk/Dialect/Polymorphic/IR/Ops.h>
+#include <mlir/Dialect/Arith/IR/Arith.h>
 #include <mlir/IR/Block.h>
 #include <mlir/IR/Value.h>
 #include <mlir/Support/LLVM.h>
@@ -17,17 +18,18 @@ Symbol SymbolicStore::lookup(mlir::Value value) {
   // Input signal
   if (auto blockArg = mlir::dyn_cast<mlir::BlockArgument>(value)) {
     // TODO: what do we do here??
-    llvm::report_fatal_error("fuck");
+    llvm::report_fatal_error("ruh-roh");
   }
 
   mlir::Operation *definingOp = value.getDefiningOp();
   return llvm::TypeSwitch<mlir::Operation *, Symbol>(definingOp)
       // A template parameter
-      .Case<llzk::polymorphic::ConstReadOp>([&](auto templRead) {
-        return pool.templ_param(templRead.getConstName());
-      })
+      .Case<llzk::polymorphic::ConstReadOp>(
+          [&](llzk::polymorphic::ConstReadOp templRead) {
+            return pool.templ_param(templRead.getConstName());
+          })
       // Read from a value array into a value scalar
-      .Case<llzk::array::ReadArrayOp>([&](auto arrRead) {
+      .Case<llzk::array::ReadArrayOp>([&](llzk::array::ReadArrayOp arrRead) {
         auto array = arrRead.getArrRef();
         // Get the indices as symbols
         std::vector<Symbol> indices;
@@ -47,5 +49,20 @@ Symbol SymbolicStore::lookup(mlir::Value value) {
         }
         return valueStore[ref];
       })
-      .Default([&](auto _) { return pool.fresh_unknown(); });
+      .Case<llzk::felt::FeltBinaryOpInterface>(
+          [&](llzk::felt::FeltBinaryOpInterface binop) {
+            char op = mlir::isa<llzk::felt::AddFeltOp>(binop)   ? '+'
+                      : mlir::isa<llzk::felt::SubFeltOp>(binop) ? '-'
+                      : mlir::isa<llzk::felt::MulFeltOp>(binop) ? '*'
+                                                                : '?';
+            if (op == '?')
+              return pool.fresh_unknown();
+            return pool.arith(lookup(binop.getLhs()), lookup(binop.getRhs()),
+                              op);
+          })
+      .Case<llzk::felt::FeltConstantOp>([&](llzk::felt::FeltConstantOp op) {
+        // TODO: this won't work for "arith.constant"
+        return pool.constant(op.getValue());
+      })
+      .Default([&](auto) { return pool.fresh_unknown(); });
 }
