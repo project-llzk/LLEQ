525e4dff518e4cc7789365c5cfbf939dd381a48b
Raghav Malik <malikraghav9@gmail.com>
2025-12-17 16:53:17 -0500

    Forwarding equality to symbol classes (#15)
    * Forwarding equality to symbol classes
    
    * Updated ref equality to not use sym pointer equality
    
    ---------
    
    Co-authored-by: Raghav Malik <raghav@veridise.com>

--- include/Analysis/SymbolExpr.h
+++ include/Analysis/SymbolExpr.h
@@ -6,7 +6,6 @@
 #pragma once
 
 #include <cstddef>
-#include <initializer_list>
 #include <llvm/ADT/APInt.h>
 #include <llvm/Support/raw_ostream.h>
 #include <memory_resource>
@@ -20,9 +19,26 @@ struct SymbolBase {
   virtual llvm::raw_ostream &print(llvm::raw_ostream &os) const = 0;
   virtual unsigned hash_value() const = 0;
 
+  bool operator==(const SymbolBase &other) const {
+    if (typeid(this) != typeid(&other)) {
+      return false;
+    }
+    return eq(other);
+  }
+
 protected:
   SymbolPool *pool;
+  virtual bool eq(const SymbolBase &other) const = 0;
 };
+
+template <class SymbolT> struct SymbolEq : SymbolBase {
+  SymbolEq(SymbolPool *pool) : SymbolBase{pool} {}
+  bool eq(const SymbolBase &other) const override {
+    return static_cast<const SymbolT &>(*this) ==
+           static_cast<const SymbolT &>(other);
+  }
+};
+
 }; // namespace impl
 
 // Represents a symbolic expression assigned to a signal (as defined on pg.11 of
--- lib/Analysis/SymbolExpr.cpp
+++ lib/Analysis/SymbolExpr.cpp
@@ -18,9 +18,9 @@
 
 using namespace lleq;
 
-struct Unknown : impl::SymbolBase {
+struct Unknown : impl::SymbolEq<Unknown> {
   size_t n;
-  Unknown(SymbolPool *pool, size_t n) : impl::SymbolBase{pool}, n{n} {}
+  Unknown(SymbolPool *pool, size_t n) : impl::SymbolEq<Unknown>{pool}, n{n} {}
   llvm::raw_ostream &print(llvm::raw_ostream &os) const override {
     os << '?' << n;
     return os;
@@ -28,12 +28,13 @@ struct Unknown : impl::SymbolBase {
   unsigned hash_value() const override {
     return llvm::hash_combine("unknown", n);
   }
+  bool operator==(const Unknown &other) const { return n == other.n; }
 };
 
-struct Constant : impl::SymbolBase {
+struct Constant : impl::SymbolEq<Constant> {
   mlir::APInt value;
   Constant(SymbolPool *pool, mlir::APInt value)
-      : impl::SymbolBase{pool}, value{value} {}
+      : impl::SymbolEq<Constant>{pool}, value{value} {}
   llvm::raw_ostream &print(llvm::raw_ostream &os) const override {
     // Print it as signed
     value.print(os, true);
@@ -42,12 +43,13 @@ struct Constant : impl::SymbolBase {
   unsigned hash_value() const override {
     return llvm::hash_combine("constant", value);
   }
+  bool operator==(const Constant &other) const { return value == other.value; }
 };
 
-struct TemplParam : impl::SymbolBase {
+struct TemplParam : impl::SymbolEq<TemplParam> {
   std::string name;
   TemplParam(SymbolPool *pool, llvm::StringRef name)
-      : impl::SymbolBase{pool}, name{name} {}
+      : impl::SymbolEq<TemplParam>{pool}, name{name} {}
   llvm::raw_ostream &print(llvm::raw_ostream &os) const override {
     os << '@' << name.data();
     return os;
@@ -55,16 +57,18 @@ struct TemplParam : impl::SymbolBase {
   unsigned hash_value() const override {
     return llvm::hash_combine("template", name);
   }
+  bool operator==(const TemplParam &other) const { return name == other.name; }
 };
 
-struct Index : impl::SymbolBase {
+struct Index : impl::SymbolEq<Index> {
   mlir::Value signal;
   llvm::SmallVector<Symbol> indices;
 
   Index(SymbolPool *pool, mlir::Value signal, llvm::ArrayRef<Symbol> ns)
-      : impl::SymbolBase{pool}, signal{signal}, indices{ns} {}
+      : impl::SymbolEq<Index>{pool}, signal{signal}, indices{ns} {}
   llvm::raw_ostream &print(llvm::raw_ostream &os) const override {
-    os << pool->getNameForValue(signal);
+    // TODO: print the MLIR value too
+    os << "sig";
     for (auto n : indices) {
       os << "[";
       n->print(os) << "]";
@@ -76,15 +80,20 @@ struct Index : impl::SymbolBase {
         "indices", signal,
         llvm::hash_combine_range(indices.begin(), indices.end()));
   }
+  bool operator==(const Index &other) const {
+    return signal == other.signal && indices.size() == other.indices.size() &&
+           std::equal(indices.begin(), indices.end(), other.indices.begin(),
+                      [](auto *a, auto *b) { return *a == *b; });
+  }
 };
 
-struct OpCall : impl::SymbolBase {
+struct OpCall : impl::SymbolEq<OpCall> {
   llvm::SmallVector<Symbol> arguments;
   std::string opName;
 
   OpCall(SymbolPool *pool, llvm::StringRef opName,
          llvm::ArrayRef<Symbol> arguments)
-      : impl::SymbolBase{pool}, arguments{arguments}, opName{opName} {}
+      : impl::SymbolEq<OpCall>{pool}, arguments{arguments}, opName{opName} {}
 
   llvm::raw_ostream &print(llvm::raw_ostream &os) const override {
     os << opName << "(";
@@ -99,6 +108,13 @@ struct OpCall : impl::SymbolBase {
   unsigned hash_value() const override {
     return llvm::hash_combine(
         opName, llvm::hash_combine_range(arguments.begin(), arguments.end()));
+  }
+  bool operator==(const OpCall &other) const {
+    return opName == other.opName &&
+           arguments.size() == other.arguments.size() &&
+           std::equal(arguments.begin(), arguments.end(),
+                      other.arguments.begin(),
+                      [](auto *a, auto *b) { return *a == *b; });
   }
 };
 
