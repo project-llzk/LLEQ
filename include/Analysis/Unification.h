ae0aeccb536f8d505ccf4935e61bf8597bbd0db9
Raghav Malik <raghav@veridise.com>
2025-12-19 20:54:15 -0500

    Implemented basic symbol unification

---
+++ include/Analysis/Unification.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "Analysis/SymbolExpr.h"
+#include <llvm/ADT/DenseMap.h>
+#include <mlir/Support/LLVM.h>
+
+namespace lleq {
+
+using Substitutions = llvm::SmallVector<std::pair<unsigned, Symbol>>;
+
+// Attempt to unify a single pair of symbols, updating `s` with the
+// substitutions. Returns `failure()` if the symbols cannot be unified
+mlir::LogicalResult unify(Symbol a, Symbol b, Substitutions &s);
+
+// Attempt to simultaneously unify multiple pairs of symbols, updating `s` with
+// the substitutions. Returns `failure()` if any pair cannot be unified, or if
+// the substitutions disagree
+mlir::LogicalResult unify_all(llvm::ArrayRef<Symbol> as,
+                              llvm::ArrayRef<Symbol> bs, Substitutions &s);
+
+// Substitute a set of unknowns in a symbol
+Symbol substitute(Symbol original, const Substitutions &m);
+
+} // namespace lleq
---
+++ lib/Analysis/Unification.cpp
@@ -0,0 +1,119 @@
+#include "Analysis/Unification.h"
+#include "Analysis/SymbolExpr.h"
+#include "SymbolImpls.h"
+#include <llvm/ADT/STLExtras.h>
+#include <llvm/ADT/TypeSwitch.h>
+#include <mlir/Support/LLVM.h>
+
+namespace lleq {
+
+bool _occurs(unsigned u, Symbol s) {
+  return llvm::TypeSwitch<Symbol, bool>(s)
+      .Case<Unknown>([u](Unknown *s) { return s->n == u; })
+      .Case<OpCall>([u](OpCall *s) {
+        for (auto arg : s->arguments) {
+          if (_occurs(u, arg))
+            return true;
+        }
+        return false;
+      })
+      .Case<Index>([u](Index *s) {
+        for (auto idx : s->indices) {
+          if (_occurs(u, idx))
+            return true;
+        }
+        return false;
+      })
+      .Default([](auto) { return false; });
+}
+
+mlir::LogicalResult unify(Symbol a, Symbol b, Substitutions &s) {
+  // Check the easy case first
+  if (*a == *b)
+    return mlir::success();
+
+  // Different constructors can't be unified
+  using SymbolKind = impl::SymbolBase::SymbolKind;
+  if (a->kind != SymbolKind::SK_Unknown && b->kind != SymbolKind::SK_Unknown &&
+      a->kind != b->kind)
+    return mlir::failure();
+
+  // Here we know that either at least one of them is unknown, or they have the
+  // same constructor and we'll have to recurse
+
+  // If one of them is unknown, make sure its not coinductive, update the
+  // mapping, and succeed
+  if (a->kind == SymbolKind::SK_Unknown) {
+    if (_occurs(mlir::dyn_cast<Unknown>(a)->n, b))
+      return mlir::failure();
+    s.push_back({mlir::dyn_cast<Unknown>(a)->n, b});
+    return mlir::success();
+  }
+  // By symmetry
+  if (b->kind == SymbolKind::SK_Unknown) {
+    return unify(b, a, s);
+  }
+
+  // Neither is unknown, so we have to recurse
+  return llvm::TypeSwitch<Symbol, mlir::LogicalResult>(a)
+      .Case<OpCall>([b, &s](OpCall *callA) {
+        auto callB = mlir::dyn_cast<OpCall>(b);
+        if (callA->opName != callB->opName)
+          return mlir::failure();
+        return unify_all(callA->arguments, callB->arguments, s);
+      })
+      .Case<Index>([b, &s](Index *idxA) {
+        auto idxB = mlir::dyn_cast<Index>(b);
+        if (idxA->signal != idxB->signal)
+          return mlir::failure();
+        return unify_all(idxA->indices, idxB->indices, s);
+      })
+      .Default([](auto) { return mlir::failure(); });
+}
+
+mlir::LogicalResult unify_all(llvm::ArrayRef<Symbol> as,
+                              llvm::ArrayRef<Symbol> bs, Substitutions &s) {
+
+  if (as.size() != bs.size())
+    return mlir::failure();
+
+  for (auto [a, b] : llvm::zip(as, bs)) {
+    if (mlir::failed(unify(substitute(a, s), substitute(b, s), s))) {
+      return mlir::failure();
+    }
+  }
+  return mlir::success();
+}
+
+Symbol _single_subst(Symbol original, unsigned k, Symbol v) {
+  return llvm::TypeSwitch<Symbol, Symbol>(original)
+      .Case<Unknown>([k, v](Unknown *u) -> Symbol {
+        if (u->n == k)
+          return v;
+        return u;
+      })
+      .Case<OpCall>([k, v](OpCall *c) -> Symbol {
+        llvm::SmallVector<Symbol> substArgs;
+        for (auto arg : c->arguments) {
+          substArgs.push_back(_single_subst(arg, k, v));
+        }
+        return c->pool->func_call(c->opName, substArgs);
+      })
+      .Case<Index>([k, v](Index *i) -> Symbol {
+        llvm::SmallVector<Symbol> indices;
+        for (auto idx : i->indices) {
+          indices.push_back(_single_subst(idx, k, v));
+        }
+        return i->pool->index(i->signal, indices);
+      })
+      .Default([original](auto) { return original; });
+}
+
+Symbol substitute(Symbol original, const Substitutions &m) {
+  // TODO: This fills the pool up with a bunch of intermediate symbols...
+  for (auto [k, v] : m) {
+    original = _single_subst(original, k, v);
+  }
+  return original;
+}
+} // namespace lleq
