1275931998d58d2e714c02de17f24f19d4e0ba2f
Raghav Malik <malikraghav9@gmail.com>
2025-12-22 12:26:41 -0500

    13 add the debug flag and better cli parsing to the frontend (#17)
    * Frontend options
    
    * Some logging
    
    * Apply suggestions from code review
    
    Co-authored-by: Ian Neal <ian@veridise.com>
    
    * llCat -> lleqCat
    
    ---------
    
    Co-authored-by: Raghav Malik <raghav@veridise.com>
    Co-authored-by: Ian Neal <ian@veridise.com>

---
+++ include/lleq/CliOptions.h
@@ -0,0 +1,25 @@
+/**
+ * Copyright 2025 Veridise Inc.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#include <llvm/Support/CommandLine.h>
+
+namespace lleq::cli {
+extern llvm::cl::OptionCategory lleqCat;
+
+// Whether to stop after constructing the symbolic store and dump it
+[[nodiscard]] bool dumpStore();
+
+// Whether to disable the symbolic store construction phase
+[[nodiscard]] bool disableStore();
+
+// Whether to disable the deductive verification phase
+[[nodiscard]] bool disableVerifier();
+
+// The input file
+[[nodiscard]] std::string &inputFile();
+
+} // namespace lleq::cli
