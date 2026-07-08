130f29ad392b81f2cecb4519e566df52900a2527
Raghav Malik <raghav@veridise.com>
2026-07-08 14:36:08 -0400

    default to release build

--- flake.nix
+++ flake.nix
@@ -34,7 +34,10 @@
       overlays.default = final: prev: {
 
         # Default lleq build uses the default compiler for the system (usually gcc for Linux and clang for Macos)
-        lleq = (final.callPackage ./nix/lleq.nix { clang = final.clang_20; llzk = final.llzk-debug; mlir = final.mlir-debug; }).overrideAttrs(attr: {
+        lleq = (final.callPackage ./nix/lleq.nix { clang = final.clang_20; llzk = final.llzk; mlir = final.mlir; }).overrideAttrs(attr: {
+          cmakeBuildType = "RelWithDebInfo";
+        });
+        lleqDebug = (final.callPackage ./nix/lleq.nix { clang = final.clang_20; llzk = final.llzk-debug; mlir = final.mlir-debug; }).overrideAttrs(attr: {
           cmakeBuildType = "Debug";
         });
         # Build in release with symbols mode with a particular compiler and sanitizers enabled.
